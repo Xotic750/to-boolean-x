dea6c3567923b306b91eaeb741ca15712057c2fb
Graham Fairweather <xotic750@gmail.com>
2019-07-18 18:31:17 +0200

    :bookmark: v2.0.13
    Full auto build

--- dist/to-boolean-x.js
+++ dist/to-boolean-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-18T15:53:45.476Z",
+  "date": "2019-07-18T16:31:11.401Z",
   "describe": "",
   "description": "Converts argument to a value of type Boolean.",
   "file": "to-boolean-x.js",
   "hash": "2f26c083c7734e1c706f",
   "license": "MIT",
-  "version": "2.0.12"
+  "version": "2.0.13"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
