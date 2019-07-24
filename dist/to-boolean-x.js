dd610699ac6d01e1eb52d2fd70627623ef10dffa
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:12:44 +0200

    :bookmark: v2.0.19
    Add jsdelivr

--- dist/to-boolean-x.js
+++ dist/to-boolean-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-23T23:32:46.804Z",
+  "date": "2019-07-24T13:12:39.259Z",
   "describe": "",
   "description": "Converts argument to a value of type Boolean.",
   "file": "to-boolean-x.js",
   "hash": "2f26c083c7734e1c706f",
   "license": "MIT",
-  "version": "2.0.18"
+  "version": "2.0.19"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
