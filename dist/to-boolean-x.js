684b85723816d991faf3b843f7980e740b0dd266
Graham Fairweather <xotic750@gmail.com>
2019-07-31 18:59:11 +0200

    Package rework

--- dist/to-boolean-x.js
+++ dist/to-boolean-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-31T15:28:35.461Z",
+  "date": "2019-07-31T16:59:05.988Z",
   "describe": "",
   "description": "Converts argument to a value of type Boolean.",
   "file": "to-boolean-x.js",
